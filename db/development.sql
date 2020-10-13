906e567bbee7dc1267498979eae28d582e4f38dc
Matthew Gibson <mattgibson6@gmail.com>
2020-10-13 13:07:46 -0600

    add schema/seeds files in db

---
+++ db/development.sql
@@ -0,0 +1,44 @@
+
+-- ************************************************************
+-- users table
+-- ************************************************************
+insert into users
+  (username, email, password, active)
+values
+  ('mathluvr', 'pblann0@pagesperso-orange.fr', '123', true);
+insert into users
+  (username, email, password, active)
+values
+  ('dbaggiani1', 'mneeves1@theglobeandmail.com', '123', true);
+insert into users
+  (username, email, password, active)
+values
+  ('lwillmore2', 'dcurado2@chron.com', '123', true);
+insert into users
+  (username, email, password, active)
+values
+  ('dchansonnau3', 'ahemphall3@dedecms.com', '123', true);
+insert into users
+  (username, email, password, active)
+values
+  ('mulrik4', 'kcogin4@accuweather.com', '123', true);
+insert into users
+  (username, email, password, active)
+values
+  ('lkennerknecht5', 'stomlins5@boston.com', '123', true);
+insert into users
+  (username, email, password, active)
+values
+  ('nkollaschek6', 'jstarte6@themeforest.net', '123', true);
+insert into users
+  (username, email, password, active)
+values
+  ('coder4lyfe', 'ddobble7@google.co.uk', '123', true);
+insert into users
+  (username, email, password, active)
+values
+  ('eforcade8', 'ejimeno8@geocities.com', '123', true);
+insert into users
+  (username, email, password, active)
+values
+  ('dkleiser9', 'pkilborn9@skype.com', '123', true);
